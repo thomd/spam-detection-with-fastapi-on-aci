3d9fddf47bed748f10105f91edefd26d9c208ae4
Thomas Dürr <thomduerr@gmail.com>
2021-11-21 13:08:27 +0100

    train

---
+++ train.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+import re
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import cross_val_score
+from joblib import dump
+
+
+def preprocessor(text):
+    text = re.sub("<[^>]*>", "", text)
+    emoticons = re.findall("(?::|;|=)(?:-)?(?:\)|\(|D|P)", text)
+    text = re.sub("[\W]+", " ", text.lower()) + " ".join(emoticons).replace("-", "")
+    return text
+
+
+dataset = pd.read_csv("./data/spam.csv")
+X = dataset["Message"].apply(preprocessor)
+y = dataset["Category"]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+
+vectorizer = TfidfVectorizer(strip_accents=None, lowercase=False, max_features=700, ngram_range=(1, 1))
+pipeline = Pipeline([("vectorizer", vectorizer), ("nn", MLPClassifier(hidden_layer_sizes=(700, 700)))])
+pipeline.fit(X_train, y_train)
+
+y_pred = pipeline.predict(X_test)
+print(classification_report(y_test, y_pred))
+print(f"Accuracy: {100 * accuracy_score(y_test, y_pred)} %")
+
+dump(pipeline, "./model/spam-classifier.joblib")
